40c27c4dae1dcf8e4b5ca7beb1779a373af323e0
vainicius <comercial.viniciusgabriel@gmail.com>
2023-06-24 23:22:29 -0300

    CORS error fixed - marcasList OK!

--- src/views/CategoriasView.vue
+++ src/views/CategoriasView.vue
@@ -10,37 +10,16 @@
       </tr>
     </thead>
      <tbody class="table-group-divider">
-       <tr v-for="item in marcasList" :key="item.id">
-        <th scope="row"><span v-if="item.ativo" class="badge text-bg-success">ATIVO</span>
-        {{ item.id }}</th>
-        <th scope="row"><span v-if="!item.ativo" class="badge rounded-pill text-bg-danger">INATIVO</span>
-          {{ item.id }}</th>
-        <td>Peugeot</td>
-        <td><button type="button" class="btn btn-outline-danger">Editar</button>
-
-        </td>
-
-      </tr>
-      <tr>
-        <th scope="row"><span class="badge text-bg-success">ATIVO</span> 2</th>
-        <td>Volkswagem</td>
+      
+      <tr v-for="item in marcasList" :key="item.id">
+        <th scope="row"><span v-if="item.ativo" class="badge text-bg-success">ATIVO</span>{{ item.id }} </th>
+        <th scope="row"><span v-if="!item.ativo" class="badge rounded-pill text-bg-danger">INATIVO</span>{{ item.nomeMarca }} </th>
+        
         
         <td><button type="button" class="btn btn-outline-danger">Editar</button>
         </td>
       </tr>
-      <tr>
-        <th scope="row"><span class="badge rounded-pill text-bg-danger">INATIVO</span>
-          3</th>
-        <td>Renault</td>
-        <td><button type="button" class="btn btn-outline-danger">Editar</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row"><span class="badge text-bg-success">ATIVO</span> 4</th>
-        <td>Fiat</td>
-        <td><button type="button" class="btn btn-outline-danger">Editar</button>
-        </td>
-      </tr>
+  
 
     </tbody>
   </table>
