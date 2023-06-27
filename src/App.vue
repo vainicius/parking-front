8820ae5435fea912e9a12f8409e11f515379e8cc
vainicius <comercial.viniciusgabriel@gmail.com>
2023-06-26 23:19:57 -0300

    Retirando dropdown das 'Configuracoes'

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,50 @@
 <template>
-  <nav class=" navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <router-link class="navbar-brand " to="/">ParkingApp</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <router-link class="navbar-brand" to="/">ParkingApp</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse d-flex justify-content-between " id="navbarNavDropdown">
+      <div
+        class="collapse navbar-collapse d-flex justify-content-between"
+        id="navbarNavDropdown"
+      >
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/">Inicio</router-link>
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Inicio</router-link
+            >
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/marcas">Marcas</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/cadastrar">Cadastros</router-link>
-
+            <router-link class="nav-link" to="/cadastrar"
+              >Cadastros</router-link
+            >
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Configurações
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/">Action</a></li>
-              <li><a class="dropdown-item" href="/">Another action</a></li>
-              <li><a class="dropdown-item" href="">Something else here</a></li>
-            </ul>
+
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Configurações</router-link>
           </li>
         </ul>
         <router-link
-         type="button" to="/" class=" btn btn-primary btn-sm bg-success border border-success 
-">Nova movimentação </router-link>
+          type="button"
+          to="/"
+          class="btn btn-primary btn-sm bg-success border border-success"
+          >Nova movimentação
+        </router-link>
       </div>
     </div>
   </nav>
-  
 
   <router-view />
 </template>
